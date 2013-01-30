f038343ae198c747624b3870ff0a4b77c0432124
Ivan Huang <huangqiwu@shendu.com>
2013-01-30 12:28:29 +0800

    fix usb mass_storage

--- msm8660.mk
+++ msm8660.mk
@@ -113,6 +113,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.mdpcomp.logs=0 \
     debug.sf.hw=1 \
     dev.pm.dyn_samplingrate=1 \
-    ro.opengles.version=131072
+    ro.opengles.version=131072 \
+    ro.vold.umsdirtyratio=50
 
 $(call inherit-product, build/target/product/full_base_telephony.mk)
